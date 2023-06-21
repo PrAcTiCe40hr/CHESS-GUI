f49298d3e9e3d86ef5ec67d8a958121ec0c74949
Peiyong Lin <75400761+PrAcTiCe40hr@users.noreply.github.com>
2023-06-20 20:56:36 -0400

    Updated the icon of the app

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
 from src.chess_gui import ChessGui
 
 
 def main():
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("icon\\chess.svg"))
     gui = ChessGui()
     gui.show()
     sys.exit(app.exec_())
